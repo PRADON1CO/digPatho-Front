2bba4d9fa7abc2d8bd43a0e022e97a0f25a6df95
Prado Nicolas <nicolas88prado@gmail.com>
2024-09-02 22:43:54 -0300

    agregue estilo para que el componente ocupe el maximo espacio

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -5,7 +5,7 @@ import error404 from"../../assets/error404.png"
 
 const Error404 = () => {
     return (
-        <Container className='text-center py-2'>
+        <Container className='text-center py-2 mainSection'>
             <img src={error404} alt="" className='imgError404 mx-auto d-block' />
             <a to='/' className=" btn bg-dark text-white border-0 rounded-0">Volver al inicio</a>
         </Container>
