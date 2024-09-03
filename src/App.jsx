a2449a5cdf68fb8360e4f34aa31815dd1a77b9df
Prado Nicolas <nicolas88prado@gmail.com>
2024-09-02 23:19:05 -0300

    agregue el maquetado necesario para el menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Error404 from './components/pages/Error404'
+import Menu from './components/common/Menu';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
  
 
   return (
     <>
+      <Menu></Menu>
       <Error404></Error404>
     </>
   )
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+import "../../style/menu.css"
 
 const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <div className="d-flex align-items-center">
+            <img src={logo} alt="Logo" className="imgLogo" width="100px" />
+            <p>DigPatho<br /><span className="colorText tamanioTextLogo">Transforming Pathology with AI</span></p>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNav" />
+        <Navbar.Collapse id="navbarNav">
+          <Nav className="mx-auto d-flex justify-content-center w-100">
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Inicio
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Contacto
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Cáncer de Mama
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Cáncer de Próstata
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              <i className="bi bi-person"></i> Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
